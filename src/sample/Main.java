0ecf4891c7dac08e30728acbb170b105470c4181
Shantanu Mantri <smantri7>
2016-06-11 15:30:55 +0900

    First Set of Changes
    ->Title page created
    ->Music works fine

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,19 +9,23 @@ import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import model.Card;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Card card = new Card("ç°ì˙ÇÕ","Hello");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group rooot = new Group();
-        Scene scene = new Scene(rooot, 540, 210);
-        primaryStage.setTitle("Hello World");
+
+        Group g = new Group(root);
+        Scene scene = new Scene(g, 421, 257);
+        scene.getStylesheets().add("C:\\Users\\Shantanu\\Documents\\Computer Science\\Personal\\ZapCard\\src\\styles\\buttons.css");
+        primaryStage.setTitle("Zap Cards");
         primaryStage.setScene(scene);
         primaryStage.show();
         MediaView mediaView = new MediaView(card.playSound());
-        ((Group)scene.getRoot()).getChildren().add(mediaView);
+        ((Group)(scene.getRoot())).getChildren().add(mediaView);
     }
     public static void main(String[] args) {
         launch(args);
