c7d89c1d466e7a1d64d6861f81fd90df40bb6f25
rzd83091 <rzd83091@uga.edu>
2016-06-11 21:59:00 -0400

    Added ListOfCards.java

---
+++ ListOfCards.java
@@ -0,0 +1,24 @@
+package model;
+
+import model.Card;
+
+public class ListOfCards{
+
+	int size = 1;
+	private Card cat[] = new Card[size];
+	
+	private void Add(Card c){
+		size++;
+		cat[size] = c;
+	}
+	private void shuffle(int a[]){
+		int x = a.length;
+		for (int i = 0; i < x; i++) {
+		    int random = i + (int) (Math.random() * (x - i));
+		    int randomElement = a[random];
+		    a[random] = a[i];
+		    a[i] = randomElement;
+		}
+	}
+	
+}//ListOfCards
